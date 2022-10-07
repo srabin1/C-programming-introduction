4ba172fd27046bab8df975fefa5d6ee8fc0675cb
srabin1 <gw4590@wayne.edu>
2022-10-07 13:23:51 -0500

    Add project files.

---
+++ firstCode/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//using namespace std; is not working here
+void main()
+{
+	int A[5];
+	A[0] = 12;
+	A[2] = 15;
+	printf("%d\n", A[2]);
+	printf("Hello from C using vs 2022!\n");
+}
